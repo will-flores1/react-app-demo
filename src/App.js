9b87515d08b70e6ece6462f6a2f621645e83f4ea
Will Flores <willflores1221@gmail.com>
2022-07-04 14:42:13 -0400

    Init episode-5

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import './App.css';
-import Header from './components/Header/Header.js';
+import "./App.css";
+import Header from "./components/Header/Header.js";
+import SearchBar from "./components/SearchBar/SearchBar.js";
 
 function App() {
-  const name = 'wilfredo_flores();';
+	const name = "wilfredo_flores();";
 
-  return (
-    <div className="App">
-      <Header company={name} />
-    </div>
-  );
+	return (
+		<div className="App">
+			<Header company={name} />
+			<SearchBar />
+		</div>
+	);
 }
 
 export default App;
 
-  // class based components
+// class based components
 // import React from 'react';
 // export default class App extends React.Component{
 //   constructor(props) {
